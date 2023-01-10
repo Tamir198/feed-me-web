8270bf081c7d584f1b03654e3ecfeabcce75d043
Daniellez24 <daniellez1123@gmail.com>
2023-01-10 17:23:21 +0200

    getTips request

--- frontend/src/pages/App/App.js
+++ frontend/src/pages/App/App.js
@@ -15,7 +15,7 @@ function App() {
       <Header />
       <NavBar />
       {/* <Feed /> */}
-      {/* <Facts /> */}
+      {/* <Tips /> */}
       {/* <LoginMenu /> */}
     </div>
   );
--- frontend/src/pages/Tips/Tips.js
+++ frontend/src/pages/Tips/Tips.js
@@ -1,10 +1,25 @@
+import { useEffect, useState } from "react";
 import TipItem from "../../components/TipItem/TipItem";
+import { Api } from "../../services/api";
 import styles from "./Tips.module.css"
 
 function Tips() {
+
+  const [tips, setTips] = useState([])
+
+  async function getTips(){
+    const res = await Api.get("foodTips/getTips");
+    setTips(res.data)
+  }
+
+  // calling to the getTips function, once the component is loaded
+  useEffect(() => {
+    getTips()
+  }, []);
+
   return (
     <div>
-      <TipItem description="Everybody loves Pizza" />
+      {tips.allTips && tips.allTips.map(tip => <TipItem description = {tip.foodTip}/>)}
     </div>
   );
 }
