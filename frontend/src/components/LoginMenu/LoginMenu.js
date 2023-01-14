e7e8dd1612f1a0748a079bf1e5ece079058b9210
Tamir Abutbul <1tamir198@gmail.com>
2023-01-14 16:40:10 +0200

    Change ui of login buttons

--- frontend/src/components/LoginMenu/LoginMenu.js
+++ frontend/src/components/LoginMenu/LoginMenu.js
@@ -1,58 +1,57 @@
 import styles from "./LoginMenu.module.css";
-import {useEffect, useRef, useState} from "react"
+import { useEffect, useRef, useState } from "react";
 import { Api } from "../../services/api";
-import { Router, useNavigate, Routes, Navigate, Route} from "react-router-dom";
+import { Link } from "react-router-dom";
+import { Router, useNavigate, Routes, navigate, Route } from "react-router-dom";
 
 function LoginMenu() {
-
   const currentUser = {
-    isLoggedIn: false,
-    userId: '',
-    useremail: '',
-    userFbId: ''
-  }
+    isLoggedIn: true,
+    userId: "",
+    useremail: "",
+    userFbId: "",
+  };
 
-  const userEmail = useRef(null)
-  const userPass = useRef(null)
+  const userEmail = useRef(null);
+  const userPass = useRef(null);
   const navigate = useNavigate();
 
-
-  async function createUser(){
+  async function createUser() {
     const res = await Api.post("user/newUser", {
       email: userEmail.current.value,
-      password: userPass.current.value
-    })
-    console.log(res)
-    localStorage.setItem(userEmail.current.value, JSON.stringify(res.data))
+      password: userPass.current.value,
+    });
+    console.log(res);
+    localStorage.setItem(userEmail.current.value, JSON.stringify(res.data));
     currentUser.userId = res.data._id;
     currentUser.useremail = res.data.email;
     currentUser.userFbId = res.data.firebaeId;
     currentUser.isLoggedIn = true;
-    console.log(currentUser)
+    console.log(currentUser);
   }
 
-  // useEffect(() => {
-  //   if(currentUser.isLoggedIn){
-  //     	// navigate('/Feed', {replace: true})
-  //       <Navigate replace to='/Feed' />
-  //   }
-
-  //   return (() => currentUser.isLoggedIn = false)
-  // }, [currentUser.isLoggedIn])
+  useEffect(() => {
+    if (currentUser.isLoggedIn) {
+      navigate("/Feed", { replace: true });
+      // <Navigate replace to='/Feed' />
+    }
+  }, [currentUser.isLoggedIn]);
 
   return (
-      <div className="form" >
-        <input type="email" placeholder="Email" ref={userEmail} />
-        <input type="password" placeholder="Password" ref={userPass} />
+    <div className="form">
+      <input type="email" placeholder="Email" ref={userEmail} />
+      <input type="password" placeholder="Password" ref={userPass} />
+      <div className={styles.registration_buttons}>
         <button type="submit">Login</button>
         <button onClick={createUser}>Register</button>
-        <Routes>
+      </div>
+      {/* <Routes>
           <Route
             path="/"
             element={currentUser.isLoggedIn && <Navigate to="/Feed" />}
            />
-        </Routes>
-      </div>
+        </Routes> */}
+    </div>
   );
 }
 
