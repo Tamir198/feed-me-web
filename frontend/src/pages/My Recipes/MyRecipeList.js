b03cb2a90f5ff226b37c74c75999559e6793e953
Tamir <1tamir198@gmail.com>
2023-01-16 10:44:47 +0200

    Change wring componenet rendered on the screen

--- frontend/src/pages/My Recipes/MyRecipeList.js
+++ frontend/src/pages/My Recipes/MyRecipeList.js
@@ -2,19 +2,23 @@ import ReactPaginate from "react-paginate";
 import React, { useState, useEffect } from "react";
 import { Api } from "../../services/api";
 import FeedItem from "../../components/FeedItem/FeedItem";
+import MyRecipeItem from "../../components/MyRecipeItem/MyRecipeItem";
 
 export const MyRecipeList = ({ currPage, usersRecipes }) => {
+  console.log(usersRecipes);
   return (
     <div>
-      {usersRecipes.map(({ title, description, category, createdAt }) => (
-        <FeedItem
-          title={title}
-          description={description}
-          author="Danielle"
-          date={createdAt}
-          catagory={category}
-        />
-      ))}
+      {usersRecipes.map(
+        ({ title, description, category, createdAt, author }) => (
+          <MyRecipeItem
+            title={title}
+            description={description}
+            date={createdAt}
+            author={"Change this to author"}
+            category={category}
+          />
+        )
+      )}
     </div>
   );
 };
