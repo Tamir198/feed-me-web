a6e10d9e60cdf036715953dd7f417d39e61f719c
Tamir <1tamir198@gmail.com>
2023-01-16 17:29:39 +0200

    Adding page num to reicpe number requests

--- frontend/src/pages/My Recipes/usePagesInMyRecipe.js
+++ frontend/src/pages/My Recipes/usePagesInMyRecipe.js
@@ -8,7 +8,10 @@ export const usePagesInMyRecipe = () => {
   const getRecipesNumber = async () => {
     //TODO replace id with generic userId that we want to get his requests
     const res = await Api.get(`/recipe/getAllUserRecipes:`, {
-      params: { id: "63c2aca810aa10ed160be949" },
+      params: {
+        id: "63bc4d43c1f15d6b0c846fca",
+        pageNum: 2,
+      },
     });
     setTotalRecipesNumber(res.data.userRecipes.length / 5);
     setUsersRecipes(res.data.userRecipes);
