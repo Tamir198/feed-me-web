78d5db3e07abc73c17c178b2e5bca26e95c6df75
Tamir Abutbul <1tamir198@gmail.com>
2023-03-18 16:15:10 +0200

    Display data on scraping ib front end

--- frontend/src/AppRouter.js
+++ frontend/src/AppRouter.js
@@ -7,17 +7,22 @@ import UploadRecipe from "./pages/Upload/UploadRecipe";
 import MyRecipes from "./pages/My Recipes/MyRecipes";
 import Login from "./pages/Login/Login";
 import Graps from "./pages/Graphs/Graphs";
+import { News } from "./pages/News/News";
 
 export const AppRouter = ({ setIsLoggedIn }) => (
   <Suspense fallback={<>"Loading..."</>}>
     <Routes>
       <Route path="/" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
       <Route path="/Feed" element={<Feed />} />
-      <Route path="/Profile" element={<Profile setIsLoggedIn={setIsLoggedIn} />} />
+      <Route
+        path="/Profile"
+        element={<Profile setIsLoggedIn={setIsLoggedIn} />}
+      />
       <Route path="/Tips" element={<Tips />} />
       <Route path="/UploadRecipe" element={<UploadRecipe />} />
       <Route path="/MyRecipes" element={<MyRecipes />} />
       <Route path="/Statistic" element={<Graps />} />
+      <Route path="/News" element={<News />} />
     </Routes>
   </Suspense>
 );
