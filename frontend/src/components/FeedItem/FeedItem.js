ba92642849499d21727e9ff1861ffe7cc22d21dd
Daniellez24 <daniellez1123@gmail.com>
2023-03-18 17:29:29 +0200

    minor fix in items

--- frontend/src/components/FeedItem/FeedItem.js
+++ frontend/src/components/FeedItem/FeedItem.js
@@ -6,7 +6,6 @@ function FeedItem(props) {
       <div className={styles.title}>{props.title}</div>
       <div className={styles.description}>{props.description}</div>
       <div className={styles.details}>
-        <span>Author: {props.author}</span>
         <span>Date uploaded: {props.date}</span>
         <span>Catagory: {props.catagory}</span>
       </div>
--- frontend/src/components/MyRecipeItem/MyRecipeItem.js
+++ frontend/src/components/MyRecipeItem/MyRecipeItem.js
@@ -20,7 +20,6 @@ function MyRecipeItem(props) {
       <div className={styles.title}>{props.title}</div>
       <div className={styles.description}>{props.description}</div>
       <div className={styles.details}>
-        <span>Author: {props.author}</span>
         <span>Date uploaded: {props.date}</span>
         <span>Catagory: {props.catagory}</span>
       </div>
